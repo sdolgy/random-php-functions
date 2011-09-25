25891adbb3d09286eb0f077abfca5b78d4d77d85
Sasha Dolgy <sasha.dolgy@gmail.com>
2011-09-25 14:09:58 +0200

    first commit

---
+++ distance_by_haversine.php
@@ -0,0 +1,31 @@
+<?php
+function distance_by_haversine($location_one, $location_two) {
+
+	if (!isset($location_one) || !isset($location_two)) { 
+		return array('error' => 'locations must be arrays of coordinates');
+	}
+	
+	if (!is_array($location_one) || !is_array($location_two)) { 
+		return array('error' => 'locations must be arrays of coordinates');
+	} else {
+		if (!isset($location_one['latitude']) || !isset($location_two['latitude'])) { 
+			return array('error' => 'locations must include latitude key');
+		} 
+		if (!isset($location_one['longitude']) || !isset($location_two['longitude'])) { 
+			return array('error' => 'locations must include longitude key');
+		}
+	}
+	
+	$radius = 6371000; // The equilateral radius of Earth = 6,378,100 meters.  Average radius is 6,371,000
+	$latitude_distance = deg2rad($location_two['latitude'] - $location_one['latitude']);
+	$longitude_distance = deg2rad($location_two['longitude'] - $location_one['longitude']);
+	$latitude_one_radius = deg2rad($location_one['latitude']);
+	$latitude_two_radius = deg2rad($location_two['latitude']);
+	
+	$a = sin($latitude_distance/2) * sin($latitude_distance/2) + sin($longitude_distance/2) * sin($longitude_distance/2) * cos($latitude_one_radius) * cos($latitude_two_radius);
+	$c = 2 * atan2(sqrt($a), sqrt(1-$a));
+	$distance = $radius * $c;
+	return array('meters' => round($distance,2), 'kilometers' => round($distance/1000, 2));
+	return $distance;
+}
+?>
